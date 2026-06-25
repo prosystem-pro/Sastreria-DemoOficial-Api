1de0eb3ed0a8c9a5879a44e70d0253b949ab47a4
prosystem-pro <prosystem490@gmail.com>
2026-06-25 13:02:19 -0600

    Prueba finalizada de backups

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const EvaluarYProcesarMesAntiguo = async () => {
   }
 };
 
-cron.schedule('*/1 * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
   try {
     const { contenidoSQL, nombreArchivo, resumen } = await ServicioBackup.RespaldoCompleto();
     const archivoSubido = await ServicioDrive.SubirArchivoRespaldo(nombreArchivo, contenidoSQL);
